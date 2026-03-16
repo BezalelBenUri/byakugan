66c3e3d0fbfda25eb6744f2c30b83a9b1914252d
Augustus Inyang <diamondinyang@gmail.com>
2026-03-16 16:55:26 +0100

    rays accross frame

--- src/byakugan_app/io/__init__.py
+++ src/byakugan_app/io/__init__.py
@@ -2,9 +2,14 @@
 """Input/output helpers for panorama, depth, and capture metadata workflows."""
 
 from .capture_sequence import CaptureFramePose, StitchedCapture, discover_stitched_capture
+from .raw_capture import RawCapture, RawRayObservation, discover_raw_capture, build_raw_observation
 
 __all__ = [
     "CaptureFramePose",
     "StitchedCapture",
     "discover_stitched_capture",
+    "RawCapture",
+    "RawRayObservation",
+    "discover_raw_capture",
+    "build_raw_observation",
 ]
